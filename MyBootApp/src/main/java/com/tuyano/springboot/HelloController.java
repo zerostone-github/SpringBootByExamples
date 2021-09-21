5cef8776fc9894f127c74de07e8b0d970541f752
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-21 22:04:52 +0900

    3-10

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,13 +1,10 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-	String[] names = {"kim", "lee","park","choi","jo"};
-	String[] mails = {"kim@aaa.com", "lee@aaa.com","park@aaa.com","choi@aaa.com","jo@aaa.com"};
 
 	@RequestMapping("/")
 	public String index() {
