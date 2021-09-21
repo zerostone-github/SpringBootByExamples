293c8227c567b3f677592b568364649c569bc5d9
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-22 00:42:57 +0900

    3-16

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -3,19 +3,28 @@ package com.tuyano.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-		int res = 0;
-		for (int i=0; i<num ; i++) {
-			res += i;
-		}
-		mav.addObject("msg", "total: " + res);
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg", "이름을 적어서 전송해주세요.");
+		return mav;
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1") String str, ModelAndView mav) {
+		
+		mav.addObject("msg","안녕하세요! " + str + "님!");
+		mav.addObject("value", str);
+		
+		mav.setViewName("index");
+		
 		
 		return mav;
 	}
