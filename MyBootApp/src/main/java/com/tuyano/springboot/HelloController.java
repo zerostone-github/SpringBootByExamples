b9d4c7fc76fb1af5ab1e3c6a5a6b44531f317acf
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-21 23:33:48 +0900

    3-13

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,13 +1,21 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int res = 0;
+		for (int i=0; i<num ; i++) {
+			res += i;
+		}
+		model.addAttribute("msg", "total: " + res);
+		
 		return "index";
 	}
 	
