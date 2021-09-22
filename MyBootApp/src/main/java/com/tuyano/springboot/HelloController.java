bf288c31ac66596cd4e8073d67a7231f5abbbc4d
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-22 22:48:21 +0900

    5-5

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,25 +1,29 @@
 package com.tuyano.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tuyano.springboot.repositories.MyDataRepository;
+
 @Controller
 public class HelloController {
+;
 
+	@Autowired
+	MyDataRepository repository;	
+	
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		Iterable<MyData> list = repository.findAll();
+		mav.addObject("data",list);
+		
 		return mav;
 	}
 	
-	@RequestMapping("/other")
-	public String other() {
-		return "redirect:/";
-		
-	}
 
 }
