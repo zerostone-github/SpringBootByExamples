9826f815105a586bbf74f02fe79a04b4e7ad7ff9
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-22 20:36:38 +0900

    3-20

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -10,44 +10,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "품을 전송해주세요.");
 		return mav;
 	}
 	
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView send(
-			@RequestParam(value="check1", required=false) boolean check1,
-			@RequestParam(value="radio1", required=false) String radio1,
-			@RequestParam(value="select1", required=false) String select1,
-			@RequestParam(value="select2", required=false) String[] select2,
-			ModelAndView mav) {
+	@RequestMapping("/other")
+	public String other() {
+		return "redirect:/";
 		
-		String res = "";
-		try {
-			res = "check:" + check1 +
-					" radio: " + radio1 +
-					" select:" + select1 +
-					"\nselect2:";	
-		} catch (NullPointerException e) {}
-		
-		try {
-			res += select2[0];
-			for(int i = 1; i < select2.length; i++)
-				res += ", " + select2[i];
-		} catch (NullPointerException e){
-			res += "null";
-		}
-		
-		
-		mav.addObject("msg",res);
-		mav.setViewName("index");
-		
-		
-		return mav;
 	}
-	
 
 }
