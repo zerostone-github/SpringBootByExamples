0c4fd4b723aa99af9b0e3f1d11356ceead32002e
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-22 20:25:21 +0900

    3-18

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -13,16 +13,36 @@ public class HelloController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "이름을 적어서 전송해주세요.");
+		mav.addObject("msg", "품을 전송해주세요.");
 		return mav;
 	}
 	
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView send(@RequestParam("text1") String str, ModelAndView mav) {
+	public ModelAndView send(
+			@RequestParam(value="check1", required=false) boolean check1,
+			@RequestParam(value="radio1", required=false) String radio1,
+			@RequestParam(value="select1", required=false) String select1,
+			@RequestParam(value="select2", required=false) String[] select2,
+			ModelAndView mav) {
 		
-		mav.addObject("msg","안녕하세요! " + str + "님!");
-		mav.addObject("value", str);
+		String res = "";
+		try {
+			res = "check:" + check1 +
+					" radio: " + radio1 +
+					" select:" + select1 +
+					"\nselect2:";	
+		} catch (NullPointerException e) {}
 		
+		try {
+			res += select2[0];
+			for(int i = 1; i < select2.length; i++)
+				res += ", " + select2[i];
+		} catch (NullPointerException e){
+			res += "null";
+		}
+		
+		
+		mav.addObject("msg",res);
 		mav.setViewName("index");
 		
 		
