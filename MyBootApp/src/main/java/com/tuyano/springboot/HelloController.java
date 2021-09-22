5c8f30348eb175c9d7f760f6db8bb7d5177a3533
70231 <70231@DESKTOP-5HB8GI8.lgcns.com>
2021-09-22 22:56:00 +0900

    5-8

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -19,9 +19,7 @@ public class HelloController {
 	MyDataRepository repository;	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index(
-			@ModelAttribute("formModel") MyData mydata, 
-			ModelAndView mav) {
+	public ModelAndView index(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg","this is sample content.");
 		Iterable<MyData> list = repository.findAll();
@@ -31,9 +29,7 @@ public class HelloController {
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	@Transactional(readOnly=false)
-	public ModelAndView form(
-			@ModelAttribute("formModel") MyData mydata, 
-			ModelAndView mav) {
+	public ModelAndView form(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
 		repository.saveAndFlush(mydata);
 		return new ModelAndView("redirect:/");
 	}
